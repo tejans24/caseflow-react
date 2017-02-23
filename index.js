70e8bb4352ceffc22151c403c60a2d1967d0f695
Teja Naraparaju <teja.s.naraparaju@gmail.com>
2017-02-23 11:54:31 -0500

    base babel project configuration with some component information

---
+++ index.js
@@ -0,0 +1,1 @@
+import TextField from './dist/TextField'
---
+++ src/TextField/TextField.js
@@ -0,0 +1,64 @@
+import React, { PropTypes } from 'react';
+export default class TextField extends React.Component {
+  onChange = (event) => {
+    this.props.onChange(event.target.value);
+  }
+
+  render() {
+    let {
+      errorMessage,
+      label,
+      name,
+      readOnly,
+      required,
+      type,
+      value,
+      validationError,
+      invisible,
+      placeholder
+    } = this.props;
+
+    return <div className={`cf-form-textinput${invisible ? " cf-invisible" : ""}`}>
+      <label className="question-label" htmlFor={name}>
+        {label || name} {required && <span className="cf-required">Required</span>}
+      </label>
+      {errorMessage && <span className="usa-input-error-message">{errorMessage}</span>}
+      <input
+        className="cf-form-textinput"
+        name={name}
+        id={name}
+        onChange={this.onChange}
+        type={type}
+        value={value}
+        readOnly={readOnly}
+        placeholder={placeholder}
+      />
+      <div className="cf-validation">
+        <span>{validationError}</span>
+      </div>
+    </div>;
+  }
+}
+
+TextField.defaultProps = {
+  type: 'text'
+};
+
+TextField.propTypes = {
+  errorMessage: PropTypes.string,
+  invisible: PropTypes.bool,
+  label: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onChange(props) {
+    if (!props.readOnly) {
+      if (typeof props.onChange !== 'function') {
+        return new Error('If TextField is not ReadOnly, then onChange must be defined');
+      }
+    }
+  },
+  placeholder: PropTypes.string,
+  readOnly: PropTypes.bool,
+  type: PropTypes.string,
+  validationError: PropTypes.string,
+  value: PropTypes.string
+};
