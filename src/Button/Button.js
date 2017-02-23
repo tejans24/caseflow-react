487621ca77b5a236d324539a56da43ca1099e10f
Teja Naraparaju <teja.s.naraparaju@gmail.com>
2017-02-23 14:55:43 -0500

    adding a button component and exports of components

---
+++ src/Button/Button.js
@@ -0,0 +1,71 @@
+import React, { PropTypes } from 'react';
+import { loadingSymbolHtml } from '../RenderFunctions.js';
+
+export default class Button extends React.Component {
+  componentDidMount() {
+    if (this.props.type === 'submit') {
+      console.warn(`Warning! You are using a button with type submit.
+        Was this intended? Make sure to use event.preventDefault() if
+        you're using it with a form and an onClick handler`);
+    }
+  }
+
+  render() {
+    let {
+      classNames,
+      children,
+      id,
+      name,
+      disabled,
+      loading,
+      onClick,
+      type
+    } = this.props;
+
+    if (loading) {
+      classNames = classNames.filter((className) => !className.includes('usa-button'));
+
+      return <span className={classNames.join(' ')}>
+        <span className="cf-react-loading-indicator">{loadingSymbolHtml()}</span>
+      </span>;
+    }
+
+    if (!children) {
+      children = name;
+    }
+
+    if (disabled) {
+      // remove any usa-button styling and then add disabled styling
+      classNames = classNames.filter((className) => !className.includes('usa-button'));
+      classNames.push('usa-button-disabled');
+    }
+
+    return <span>
+      <button
+        id={id || `${type}-${name.replace(/\s/g, '-')}`}
+        className={classNames.join(' ')}
+        type={type}
+        disabled={disabled}
+        onClick={onClick}>
+          {children}
+      </button>
+    </span>;
+  }
+}
+
+Button.defaultProps = {
+  classNames: ['cf-submit'],
+  type: 'button'
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  classNames: PropTypes.arrayOf(PropTypes.string),
+  disabled: PropTypes.bool,
+  id: PropTypes.string,
+  linkStyle: PropTypes.bool,
+  loading: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  type: PropTypes.string
+};
---
+++ src/Button/Button.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import Button from './Button';
+
+describe('Button', () => {
+  it('renders loading icon instead of button when loading', () => {
+    let onChange = () => true;
+    const wrapper = shallow(<Button
+                              id="test-button"
+                              classNames={['usa-button-primary']}
+                              name="foo"
+                              onChange={onChange}
+                              loading={true} />);
+
+    expect(wrapper.find('.usa-button-primary')).to.have.length(0);
+    expect(wrapper.find('#test-button')).to.have.length(0);
+    expect(wrapper.find('.cf-react-loading-indicator')).to.have.length(1);
+  });
+
+  it('removes other button classes when disabled', () => {
+    let onChange = () => true;
+    const wrapper = shallow(<Button
+                              name="foo"
+                              onChange={onChange}
+                              classNames={["usa-button-primary"]}
+                              disabled={true} />);
+
+    expect(wrapper.find('.usa-button-disabled')).to.have.length(1);
+    expect(wrapper.find('.usa-button-primary')).to.have.length(0);
+  });
+
+  it('renders while not loading', () => {
+    let onChange = () => true;
+    const wrapper = shallow(<Button name="foo" onChange={onChange} loading={false} />);
+
+    expect(wrapper.find('button')).to.have.length(1);
+  });
+
+  it('calls the on change function', () => {
+    let isCalled = false;
+    let onClick = () => {
+      isCalled = true;
+    };
+    let wrapper;
+
+    wrapper = mount(<Button name="foo" onClick={onClick} loading={false} />);
+    wrapper.find('button').simulate('click');
+    expect(isCalled).to.be.eq(true);
+  });
+});
