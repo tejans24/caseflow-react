5271dcc534732984183ba58ae63d658412931aa3
Teja Naraparaju <teja.s.naraparaju@gmail.com>
2017-03-06 14:19:35 -0500

    adding a space in the footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => {
   return <footer className="cf-txt-c usa-grid cf-app-footer">
     <div>
       <div className="cf-push-left">
-        <span title="">Built</span> with <abbr title="love">&#9825;</abbr> by the
+        <span title="">Built</span> with <abbr title="love">&#9825;</abbr> by the { " " }
         <a href="http://www.va.gov/ds/">Digital Service at the <abbr title="Department of Veterans Affairs">VA</abbr></a>
       </div>
       <div className="cf-push-right">
